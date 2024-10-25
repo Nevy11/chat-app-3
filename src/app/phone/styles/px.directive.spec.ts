65f70f931d4cca7c3f8d7c329aa780db34f706b2
nevy11 <smongare2003@gmail.com>
2024-10-25 10:02:49 +0300

    used my changes

--- src/app/phone/styles/px.directive.spec.ts
+++ src/app/phone/styles/px.directive.spec.ts
@@ -2,7 +2,7 @@ import { PxDirective } from './px.directive';
 
 describe('PxDirective', () => {
   it('should create an instance', () => {
-    const directive = new PxDirective();
+    const directive = new MyDirective(renderer, elementRef);
     expect(directive).toBeTruthy();
   });
 });
