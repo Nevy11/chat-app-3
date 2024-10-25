69628caf54ac0540a97e4c19463009db51b11626
nevy11 <smongare2003@gmail.com>
2024-10-25 10:01:58 +0300

    create docs to host in github

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-T3MYMZZN.mjs";import"./chunk-PCJCBCVY.mjs";import"./chunk-HSNZNF4A.mjs";import"./chunk-RKPCVMOQ.mjs";import"./chunk-VGS2VYZ7.mjs";import"./chunk-UTEJNEE6.mjs";import"./chunk-LKNLSAVK.mjs";import"./chunk-7YZCFMGL.mjs";import"./chunk-EOFGECSF.mjs";import"./chunk-ZQLYGVRE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
